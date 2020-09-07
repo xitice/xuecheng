c51d148006af7c069567d38788762c851556e6f1
sjc <sjc_buaa@foxmail.com>
2020-09-08 00:11:10 +0800

    cmstest

---
+++ xcEduService01/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequest.java
@@ -0,0 +1,18 @@
+package com.xuecheng.framework.domain.cms.request;
+
+import lombok.Data;
+
+@Data
+public class QueryPageRequest {
+    //接受页面查询的查询条件
+    //站点id
+    private String siteId;
+    //页面id
+    private String pageId;
+    //页面名称
+    private String pageName;
+    //别名
+    private String pageAliase;
+    // 模板id
+    private String templateId;
+}
